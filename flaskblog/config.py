ee785f49a081a079d633afd6e7970878846ef28b
Sankalp Srivastava <raunaksrivastava22@gmail.com>
2022-02-25 09:13:32 +0530

    updated config filr to use sqllite

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,7 @@
 import os
 class Config():
     SECRET_KEY = os.environ.get('APP_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
